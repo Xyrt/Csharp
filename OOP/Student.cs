8f395527a5a227a5be740cfa424666b079a7e623
Xyrt <rparadize@mail.ru>
2017-09-14 12:04:03 +0300

    Create Student.cs

---
+++ OOP/Student.cs
@@ -0,0 +1,43 @@
+   //public partial class Student
+    //{
+    //    public int i = 0;
+    //    public static string s = "Skolko eto mojet prodoljats9?";
+
+    //    public void SayHello(string name = "Ofc, welcome to hell my friend")
+    //    {
+    //        Console.WriteLine("9 otchislen?\n" + name);
+    //    }
+
+    //    public string Answer(string answer)
+    //    {
+    //        return "Togda 9 pokonchy s soboi";
+    //    }
+
+    //    public int GetAge() => 15; // - равносильно как и public GetAge()
+    //                               //{
+    //                               //  return 15;
+    //                               //}
+
+    //    public string name = "kk";
+    //    public int age = 14;
+
+    //    public Student(string name)
+    //    {
+    //        this.name = name;
+    //    }
+    //    public Student(string name, int age) : this(name)
+    //    {
+    //        this.age = age;
+    //    }
+
+    //    public bool TryAnswer(ref string tryAnswer)
+    //    {
+    //        tryAnswer = "42";
+    //        return true;
+    //    }
+
+    //    public void SaySomething(params string[] names)
+    //    {
+    //        Console.WriteLine("Somebody" + string.Join(" ", names));
+    //    }
+    //}
