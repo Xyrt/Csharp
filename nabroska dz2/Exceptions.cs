b02fa284d02ebb4312740bd4fc8a2b2a58c4b592
Xyrt <rparadize@mail.ru>
2017-09-19 13:13:31 +0300

    Create Exceptions.cs

---
+++ nabroska dz2/Exceptions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    public class CreditCardException : Exception
+    {
+        public int RemainingAttempts { get; set; }
+
+        [Serializable]
+        public class MyException : Exception
+        {
+            public MyException() { }
+            public MyException(string message) : base(message) { }
+            public MyException(string message, Exception inner) : base(message, inner) { }
+            protected MyException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        }
+    }
+}
