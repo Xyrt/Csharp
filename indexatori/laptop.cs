3a339204061a00eee4e90493bde42959d2b3662f
Xyrt <rparadize@mail.ru>
2017-09-19 12:09:56 +0300

    Create laptop.cs

---
+++ indexatori/laptop.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexatori
+{
+    public class Laptop
+    {
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+    }
+}
