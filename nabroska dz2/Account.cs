c3966ce47eaa9e1c286a233bf604fb68584c5f31
Xyrt <rparadize@mail.ru>
2017-09-19 13:17:18 +0300

    Create Account.cs

---
+++ nabroska dz2/Account.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    class Account
+    {
+        private Account[] accounts;
+        private CreditCard CreditCard()
+        {
+            get
+            {
+            }
+            set
+            {
+            }
+        }
+        public decimal Balance()
+        {
+        }
+    }
+}
