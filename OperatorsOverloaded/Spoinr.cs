e43e7470b3431112e302ec4b8526f41a4eb99a9d
Xyrt <rparadize@mail.ru>
2017-09-20 12:14:12 +0300

    Create Spoinr.cs

---
+++ OperatorsOverloaded/Spoinr.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson7
+{
+    public struct Spoint
+    {
+        public int X{ get; set; }
+        public int Y { get; set; }
+
+        public Spoint(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
