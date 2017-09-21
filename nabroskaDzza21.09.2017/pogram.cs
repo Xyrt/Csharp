d1db63e4e273c8eee02af4610aec5c8d222dfc52
Xyrt <rparadize@mail.ru>
2017-09-21 10:54:36 +0300

    Create pogram.cs

---
+++ nabroskaDzza21.09.2017/pogram.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Point p1 = new Point(10, 10);
+            //var p2 = new Point(20, 20);
+            //while (p1 || p2)
+            //{
+            //    p1--;
+            //    p2--;
+            //    Console.WriteLine(p1);
+            //    Console.WriteLine(p2);
+            //}
+            //Square s = 10;
+            //Rectongle r = new Rectongle()
+            //{
+            //    Height = 10,
+            //    Wight = 100
+            //};
+            //s = (Square) r;
+            Fraction f = new Fraction(3, 4);
+            int a = 10;
+            Fraction f1 = f * a;
+            Fraction f2 = a * f;
+            double d = 1.5;
+            Fraction f3 = f + d;
+        }
+
+    }
+}
