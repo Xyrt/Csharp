6f1bf67f7b9f02c0e55702a05ad6c7278072549b
Xyrt <rparadize@mail.ru>
2017-09-21 10:57:24 +0300

    Create rectongle.cs

---
+++ implicit_explicit/rectongle.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEra
+{
+    class Rectongle
+    {
+        public int Height { get; set; }
+        public int Wight { get; set; }       
+    }
+}
