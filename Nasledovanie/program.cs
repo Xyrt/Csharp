ac08fb9b34609c34fc554c091480ab4f57683ef3
Xyrt <rparadize@mail.ru>
2017-09-21 13:17:25 +0300

    Create program.cs

---
+++ Nasledovanie/program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masledovanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var student = new Student();
+            //Human h = null;
+            //h = new Student();
+            //var st = (Student)h;
+            //if (h is Student)
+            //{
+            //}
+            //Console.WriteLine(h is Student);
+            //h = new Teacher();
+            //Console.WriteLine(h is Student);
+            //var st1 = h as Student;
+            //Console.WriteLine(st1 == null);
+            //var st2 = h as Student;
+            //Console.WriteLine(st2);
+            //h = st;
+            //h.SayHello();
+            object o = 5;
+            Console.WriteLine(o);
+        }
+    }
+}
