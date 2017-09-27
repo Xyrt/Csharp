341a4453eb92d6e6564eb11358b43a104ae46989
Xyrt <rparadize@mail.ru>
2017-09-27 13:13:58 +0300

    Create Student.cs

---
+++ List/Student.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson11_1
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDay { get; set; }
+        public override int GetHashCode()
+        {
+            return (Name + Surname).GetHashCode();
+        }
+    }
+}
